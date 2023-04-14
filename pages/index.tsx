b4d3176f7c8ad3bd497d08a59ccb65da97848afc
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-04-14 15:52:47 +0100

    feat: add notice

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import Head from 'next/head'
+import { Poppins } from "@next/font/google"
 
+const poppins = Poppins({ weight: "400", subsets: ["latin"] })
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Quran Firebase</title>
+        <title>LinguistPro UI  |  By Asam</title>
         <meta name="description" content="Top mangas apps" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        Hello World
+        <div className='hidden lg:flex bg-black py-6'>
+          <p className={`text-center ${poppins.className} text-white w-full text-base`}>Don&apos;t Just Learn, Ninja Up Your Languages Skills with LinguistPro</p>
+        </div>
       </main>
     </>
   )
